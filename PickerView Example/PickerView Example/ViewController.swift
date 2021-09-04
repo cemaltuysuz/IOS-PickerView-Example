8a6360b7dd2931b47204ee76503e25eab0e8daa8
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-09-04 17:10:55 +0300

    views bind | code

--- PickerView Example/PickerView Example/ViewController.swift
+++ PickerView Example/PickerView Example/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource{
+    @IBOutlet weak var Result: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
 
+    var countries = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries = ["Turkey","Germany","England","Russia","Italia"]
+        pickerView.delegate = self
+        pickerView.dataSource = self
     }
 
-
+    @IBAction func selectButton(_ sender: Any) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return countries.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return countries[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        Result.text = countries[row]
+    }
 }
 
